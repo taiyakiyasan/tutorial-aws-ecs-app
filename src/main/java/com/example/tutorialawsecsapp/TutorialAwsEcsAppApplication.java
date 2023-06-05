079ac63b525f06031cf5075a4c271eea51b1de0f
hiroki-na <Hiroki12.Nakamura@nttdata.com>
2023-06-05 23:47:15 +0900

    init

---
+++ src/main/java/com/example/tutorialawsecsapp/TutorialAwsEcsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialawsecsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialAwsEcsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialAwsEcsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tutorialawsecsapp/TutorialAwsEcsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorialawsecsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialAwsEcsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
